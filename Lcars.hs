dc204d1a4468f36f533574c5181c29f4763d9667
Markus Barenhoff <alios@alios.org>
2012-12-14 23:49:49 +0100

    first code snippets

--- Lcars.hs
+++ Lcars.hs
@@ -1,2 +1,9 @@
-module Lcars () where
 
+module Lcars (lcarsMain) where
+
+import Control.Distributed.Process
+import qualified Control.Distributed.Backend.P2P as P2P
+
+
+lcarsMain :: Process () 
+lcarsMain = undefined
--- Main.hs
+++ Main.hs
@@ -1,7 +1,12 @@
-
 module Main (main) where
 
+import qualified Control.Distributed.Backend.P2P as P2P
+import           Control.Monad.Trans (liftIO)
+import           Control.Concurrent (threadDelay)
+
+import Lcars (lcarsMain)
+
 main :: IO ()
-main = do
-  
+main = do    
+  P2P.bootstrap "localhost" "9001" [P2P.makeNodeId "localhost:9000"] lcarsMain
   return ()
